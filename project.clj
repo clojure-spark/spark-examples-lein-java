a3e625ce600c3157c932903e8a466662935c0a44
Steve Chan <chanshunli@gmail.com>
2017-08-17 16:35:59 +0800

    把整个Java项目都端到Lein的Clojure项目里面,把Java项目的pom.xml依赖改成project.clj, main改成Java的main

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; from spark-examples project pom.xml
+                 [org.apache.spark/spark-core_2.10 "1.6.0"]
+                 [org.apache.spark/spark-mllib_2.10 "1.6.0"]
+                 [org.apache.nifi/nifi-spark-receiver "1.3.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.4.1"]]
+  :java-source-paths ["java_src"]
+  :aot :all
+  :main com.jeremydyer.Main)
