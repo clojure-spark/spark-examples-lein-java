b391288c679f530452c07d9a1ca3d31991039c12
Steve Chan <chanshunli@gmail.com>
2017-08-17 17:47:22 +0800

    Update spark-examples-lein-java.core

--- src/spark_examples_lein_java/core.clj
+++ src/spark_examples_lein_java/core.clj
@@ -1,6 +1,15 @@
-(ns spark-examples-lein-java.core)
+(ns spark-examples-lein-java.core
+  (:import
+   (org.apache.nifi.remote Transaction)
+   (org.apache.nifi.remote TransferDirection)
+   (org.apache.nifi.remote.client SiteToSiteClient)
+   (org.apache.nifi.remote.client SiteToSiteClientConfig)
+   (java.io IOException)
+   (java.util HashMap)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; java_src/main/java/com/jeremydyer/Test.java
+#_(doto (SiteToSiteClient.)
+  (.Builder)
+  (.url "http://127.0.0.1:8080/nifi")
+  (.portName "Data From Spark")
+  (.buildConfig))
